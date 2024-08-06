dfd5ee026f449e1ff3fcebdbbf447b4b7e0b1e02
teebin2019 <teebin2019@gmail.com>
2024-08-06 13:49:43 +0700

    Update

--- src/Users.js
+++ src/Users.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -8,54 +8,64 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
-import { Container } from '@mui/system';
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+import { Container } from "@mui/system";
 
 export default function BasicTable() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    UsersGet();
+  }, []);
+
+  const UsersGet = () => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((result) => {
+        setUsers(result);
+      });
+  };
+
+  // ลบข้อมูล
+  const UsersDelete = (id) => {
+    fetch("http://localhost:5000/users/" + (id), {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        UsersGet();
+      });
+  };
   return (
     <Container>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-              <TableCell align="right">Action</TableCell>
+              <TableCell align="center">ลำดับ</TableCell>
+              <TableCell align="right">ชื่อจริง</TableCell>
+              <TableCell align="right">นามสกุล</TableCell>
+              <TableCell align="center">Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.map((row) => (
+            {users.map((row) => (
               <TableRow
-                key={row.name}
+                key={row.fname}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {row.id}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-                <TableCell align="right">
+                <TableCell align="right">{row.fname}</TableCell>
+                <TableCell align="right">{row.lname}</TableCell>
+                <TableCell align="center">
                   <ButtonGroup
                     variant="contained"
                     aria-label="Basic button group"
                   >
                     <Button color="primary">Edit</Button>
-                    <Button color="error">Delete</Button>
+                    <Button color="error" onClick={() => UsersDelete(row.id)}>
+                      Delete
+                    </Button>
                   </ButtonGroup>
                 </TableCell>
               </TableRow>
